a24506d1794a2f99d7a8cca2767ddf6534c88c27
lamb0693 <mar189@naver.com>
2023-12-11 12:07:19 +0900

    upload image complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,7 @@ import 'package:http/http.dart' as http;
 
 import 'board.dart';
 import 'board_list_view.dart';
+import 'package:path/path.dart' as path;
 
 void main() {
   runApp(const MyApp());
@@ -187,11 +189,23 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _pickImage() async {
+    print('>>>> pick Image called');
     XFile? image = await picker.pickImage(source: ImageSource.camera);
     print('image file path : ${image?.path}');
 
+    if(storedAccessToken == null || storedAccessToken.isEmpty) {
+      print(">>>> not login state");
+      return;
+    }
+
     if (image != null) {
-      var uploader = Uploader("사진파일", storedAccessToken, storedTel, "IMAGE", image.path);
+      File imageFile = File(image.path);
+
+      String modifiedPath = path.join(path.dirname(imageFile.path), 'saveImg.jpg');
+      await imageFile.rename(modifiedPath);
+      print('imageFile path : ${imageFile.path}');
+
+      var uploader = Uploader("사진파일", storedAccessToken, storedTel, "IMAGE", modifiedPath);
       try {
         await uploader.upload();
         print('Upload successful');
--- lib/uploader.dart
+++ lib/uploader.dart
@@ -11,35 +11,40 @@ class Uploader {
   Uploader( this.message, this.storedAccessToken, this.storedTel,this.content, this.filePath );
 
   Future<void> upload() async {
-    try {
-      var request = http.MultipartRequest(
-        'POST',
-        Uri.parse('http://10.100.203.62:8080/api/board/create'),
-      );
+    if(storedAccessToken == null ||  storedAccessToken.isEmpty) {
+      print(">>>> not login state");
+      return;
+    } else {
+      try {
+        var request = http.MultipartRequest(
+          'POST',
+          Uri.parse('http://10.100.203.62:8080/api/board/create'),
+        );
 
-      // Set headers
-      request.headers['Authorization'] = 'Bearer $storedAccessToken';
+        // Set headers
+        request.headers['Authorization'] = 'Bearer $storedAccessToken';
 
-      // Add fields to the request
-      request.fields['customerTel'] = storedTel;
-      request.fields['tel'] = storedTel;
-      request.fields['content'] = content;
-      request.fields['message'] = message;
+        // Add fields to the request
+        request.fields['customerTel'] = storedTel;
+        request.fields['tel'] = storedTel;
+        request.fields['content'] = content;
+        request.fields['message'] = message;
 
-      // Add file to the request
-      if (filePath != null && filePath.isNotEmpty) {
-        var file = await http.MultipartFile.fromPath('file', filePath);
-        request.files.add(file);
-      }
+        // Add file to the request
+        if (filePath != null && filePath.isNotEmpty) {
+          var file = await http.MultipartFile.fromPath('file', filePath);
+          request.files.add(file);
+        }
 
-      var response = await http.Response.fromStream(await request.send());
+        var response = await http.Response.fromStream(await request.send());
 
-      if (kDebugMode) {
-        print(response.body);
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print(e);
+        if (kDebugMode) {
+          print(response.body);
+        }
+      } catch (e) {
+        if (kDebugMode) {
+          print(e);
+        }
       }
     }
   }
