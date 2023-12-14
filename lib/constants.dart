196de5b8fce78c2d57ba910b4990f08f5776f8d2
lamb0693 <mar189@naver.com>
2023-12-14 12:24:59 +0900

    final

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,10 @@
 // constants.dart
 
 class AppConstants {
-  static const String apiBaseUrl = 'http://10.100.203.62:8080';
-  static const signalSERVER = 'http://10.100.203.62:3002';
+  // static const String apiBaseUrl = 'http://10.100.203.62:8080';
+  // static const signalSERVER = 'http://10.100.203.62:3002';
+  // static const stunServer = 'stun:stun.l.google.com:19302';
+  static const String apiBaseUrl = 'http://15.164.61.120:8008';
+  static const signalSERVER = 'http://15.164.61.120:8010';
   static const stunServer = 'stun:stun.l.google.com:19302';
 }
